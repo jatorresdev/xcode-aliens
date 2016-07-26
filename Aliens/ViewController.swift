6d9b01124dd3437a953487d7bc021dd710779bf0
Angel Torres <jatorresdev@gmail.com>
2016-07-25 23:45:47 -0500

    Funcionalidad Hooking storyboards. Suma puntos al dar clic en botones

--- Aliens/ViewController.swift
+++ Aliens/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var puntaje: Int = 0
+    
+    @IBOutlet weak var label: UILabel!
+    @IBAction func disparoAlien1(sender: AnyObject) {
+        puntaje += 1
+        label.text = "Puntaje: \(puntaje)"
+    }
+    @IBAction func disparoAlien2(sender: AnyObject) {
+        puntaje += 5
+        label.text = "Puntaje: \(puntaje)"
+    }
+    @IBAction func disparoAlien3(sender: AnyObject) {
+        puntaje += 10
+        label.text = "Puntaje: \(puntaje)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
